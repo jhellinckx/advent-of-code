d77876cf3df3336d7ba8884b33442178d3ae0e34
Jérôme Hellinckx <jerome.hellinckx@famoco.com>
2025-12-05 12:54:47 +0100

    Day 5 2025

---
+++ 2025/day05/puzzle.py
@@ -0,0 +1,39 @@
+def read_input(filename="input.txt"):
+    ranges = []
+    ids = []
+    with open(filename) as f:
+        while (l := f.readline().strip()) != "":
+            ranges.append(tuple(int(d) for d in l.split("-")))
+        ids = [int(d.strip()) for d in f.readlines()]
+    return list(set(ranges)), ids
+
+
+def puzzle1():
+    ranges, ids = read_input()
+    return len({i for i in ids for l, u in ranges if l <= i <= u})
+
+
+def remove_overlaps(ranges):
+    for i, (l1, u1) in enumerate(ranges):
+        for j, (l2, u2) in enumerate(ranges):
+            if i != j:
+                if l1 == l2 and u1 == u2:
+                    return remove_overlaps(ranges[:i] + ranges[i + 1 :])
+                if l2 <= l1 <= u2:
+                    if u1 <= u2:
+                        return remove_overlaps(ranges[:i] + ranges[i + 1 :])
+                    ranges[i] = (u2 + 1, u1)
+                    return remove_overlaps(ranges)
+                if l2 <= u1 <= u2:
+                    ranges[i] = (l1, l2 - 1)
+                    return remove_overlaps(ranges)
+    return ranges
+
+
+def puzzle2():
+    ranges, _ = read_input()
+    return sum(u - l + 1 for l, u in remove_overlaps(ranges))
+
+
+if __name__ == "__main__":
+    print(puzzle2())
